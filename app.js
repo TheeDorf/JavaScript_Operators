d07c98a4d042cf2d90848596ec9fe9c70a53d85c
AJ Dorfman <ajdorfman91@gmail.com>
2023-03-10 09:23:21 -0800

    finish exercise

--- app.js
+++ app.js
@@ -4,16 +4,73 @@ console.log(
 );
 
 // Exercise 1
-console.log("EXERCISE 1:\n==========\n");
+console.log( "add:==========");
+console.log("minus: =======")
+console.log("multiply" )
+console.log("dividing")
 
 // YOUR CODE HERE
+var a = 20;
+var b = 4;
+var add = a + b;
+var minus = a-b;
+var multiply = a*b;
+var dividing = a/b; 
+console.log(add);
+console.log(minus);
+console.log(multiply);
+console.log(dividing);
+
 
 // Exercise 2
-console.log("EXERCISE 2:\n==========\n");
+console.log("EXERCISE 2:\n_________\n");
 
 // YOUR CODE HERE
+let num = 11;
+let str = "11";
+let str2 = "eleven";
+let isPresent = true;
+let firstName = "Frodo";
+let lastName = "Baggins";
+
+console.log(num+str);
+// 1111
+console.log(num+str2) ;
+// 11eleven
+console.log(num+isPresent);
+// 11
+console.log(firstName+num);
+// Frodo11
+console.log(isPresent+str);
+// 11
+console.log(firstName+lastName);
+// Frodo Baggins
+
+
+
 
 // Exercise 3
 console.log("EXERCISE 3:\n==========\n");
 
 // YOUR CODE HERE
+let val = 5;
+let str3 = "5";
+let str4 = "five";
+let isAwake = false;
+
+console.log(val == str3);
+// true
+console.log(val===str3);
+// false
+console.log(!isAwake);
+// true
+console.log("eleven"|| isAwake);
+// eleven
+console.log(0 == false);
+// true
+console.log(0===false);
+// false
+console.log(0!=false);
+// false
+console.log(0!==false);
+// true
